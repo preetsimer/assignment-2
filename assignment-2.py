86a8be7233bbab58d93b265c5fd2a1441fc77414
preetsimer <simerpreet1851.cse16@chitkara.edu.in>
2018-08-17 18:12:42 +0530

    Assignment-2

--- assignment-2.py
+++ assignment-2.py
@@ -1,12 +1,12 @@
 #question 1
 print('Hello!')
 #question 2
-print("Simer"+"preet")
+print("Acad"+"View")
 #question 3
 a=input("Enter first variable: ")
 b=input("Enter second variable: ")
 c=input("Enter third variable: ")
-print(a,b,c)
+print("The values entered are %s,%s,%s" %(a,b,c))
 #question 4
 print("Let's get staeted")
 #question 5
@@ -15,6 +15,7 @@ course="Python"
 fee=5000
 print("Fee for %s course in %s is Rs.%d" %(s,course,fee))
 #question 6
-name="Tony Stark"
-salary=10000000
-print("%s""%d"%(name,salary))
+pi=3.14
+r=float(input("Enter radius of circle "))
+area=pi*r*r
+print("Area of circle is :",area)
